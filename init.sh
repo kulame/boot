5a045628fd8d9880d69784ef13a6cf7f5105dfb9
kula <kula@live.com>
2021-10-19 18:11:30 +0800

    feat: fixed

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
-sudo su - 
-apt update
+apt update and 
 apt-get -y install zsh build-essential libcairo2-dev libpango1.0-dev libjpeg-dev libgif-dev librsvg2-dev
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
